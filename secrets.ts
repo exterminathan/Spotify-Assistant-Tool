b3ffa687e879298a7e768608dd30d2a4e585117b
Nathan Shturm <exterminathan@gmail.com>
2024-12-20 14:24:02 -0800

    Added credentials file
    No, I am not leaking credentials on GitHub. This only contains redirect URLs and the Spotify clientID, which can be public for the PKCE workflow

---
+++ secrets.ts
@@ -0,0 +1,9 @@
+// secret.ts
+
+export const secrets = {
+    clientId: '7e524fae1f6b426bbb13bc3770d0ed8f', 
+    redirectUri: {
+        dev: 'http://localhost:5173/',
+        prod: 'https://exterminathan.github.io',
+    },
+};
